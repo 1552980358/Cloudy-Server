7ec684476bc7b638380a95a1c2783a51169e1d58
1552980358 <1552980358@qq.com>
2023-12-11 14:09:30 +0800

    API: Update /account (GET)

--- src/api/account.rs
+++ src/api/account.rs
@@ -1,5 +1,9 @@
 use Rocket::Route;
 
+#[path = "account/account-get.rs"]
+mod account_get;
+use account_get::get;
+
 const ROUTE_ACCOUNT: &str = "/account";
 pub fn route<'a>() -> &'a str {
     ROUTE_ACCOUNT
@@ -7,5 +11,6 @@ pub fn route<'a>() -> &'a str {
 
 pub fn routes() -> Vec<Route> {
     routes![
+        get,
     ]
 }
