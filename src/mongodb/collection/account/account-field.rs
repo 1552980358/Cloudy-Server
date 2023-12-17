93b71a62ab8f0c6fc95aa37e692b3d0347413ac2
1552980358 <1552980358@qq.com>
2023-12-18 03:52:24 +0800

    MongoDB: Add method `has_owner` (Account/FindOwner)

--- src/mongodb/collection/account/account-field.rs
+++ src/mongodb/collection/account/account-field.rs
@@ -8,6 +8,11 @@ pub fn password<'a>() -> &'a str {
     FIELD_PASSWORD
 }
 
+const FIELD_ROLE: &str = "role";
+pub fn role<'a>() -> &'a str {
+    FIELD_ROLE
+}
+
 const FIELD_AVATAR: &str = "avatar";
 pub fn avatar<'a>() -> &'a str {
     FIELD_AVATAR
--- src/mongodb/collection/account/account-find-owner.rs
+++ src/mongodb/collection/account/account-find-owner.rs
@@ -1,27 +1,42 @@
 use mongodb::bson::{doc, to_bson};
 use mongodb::Collection;
+use mongodb::error::Result;
 
-use crate::mongodb::collection::{Account, account::Role};
+use crate::mongodb::collection::Account;
+use crate::mongodb::collection::account::{Role, field as AccountField};
 
 #[async_trait]
 pub trait FindOwner {
-    async fn find_owner(&self) -> bool;
+
+    async fn find_owner(&self) -> Result<Option<Account>>;
+
+    async fn has_owner(&self) -> bool;
+
 }
 
-const FILTER_ROLE: &str = "role";
 #[async_trait]
 impl FindOwner for Collection<Account> {
-    async fn find_owner(&self) -> bool {
-        let Ok(role) = to_bson(&Role::Owner) else {
-            return false;
-        };
+
+    async fn find_owner(&self) -> Result<Option<Account>> {
+        let role = to_bson(&Role::Owner).map_err(|_| {
+            use mongodb::error::Error;
+            use mongodb::error::ErrorKind::BsonSerialization;
+            Error::custom(BsonSerialization)
+        })?;
+
         let filter = doc! {
-            FILTER_ROLE: role
+            AccountField::role(): role,
         };
 
-        self.find_one(filter, None).await
-            .ok()
-            .map(|account| account.is_some())
-            .unwrap_or_else(|| false)
+        self.find_one(filter, None)
+            .await
     }
+
+    async fn has_owner(&self) -> bool {
+        self.find_owner()
+            .await
+            .map(|account| account.is_some())
+            .unwrap_or_else(|_| false)
+    }
+
 }
