55d57f2f5edee175dc2a626dbe420b3bf033d2c4
1552980358 <1552980358@qq.com>
2024-01-25 19:25:03 +0800

    Ut Util: Update macro concat_vec (vec)

---
+++ src/util/vec.rs
@@ -0,0 +1,11 @@
+#[macro_export]
+macro_rules! concat_vec {
+    [$elem:expr; $n:expr] => (
+        Vec::from_elem($elem, $n).concat()
+    );
+    [$($x:expr),+ $(,)?] => (
+        vec![$($x),+].concat()
+    );
+}
+
+pub use concat_vec;
