276e623d3768e201eb149c74961d5e5b49e1bb66
1552980358 <1552980358@qq.com>
2024-01-26 21:51:09 +0800

    JWT: Optimize auth request handling

--- src/jwt/jwt_auth.rs
+++ src/jwt/jwt_auth.rs
@@ -1,4 +1,4 @@
-use jsonwebtoken::errors::ErrorKind;
+use regex::Regex;
 use rocket::http::Status;
 use Rocket::outcome::IntoOutcome;
 use rocket::Request;
@@ -11,86 +11,54 @@ use crate::rocket::RequestHeader;
 
 pub struct Auth(pub String);
 
-#[derive(Debug)]
-pub enum AuthError {
-    MissingAuthorization,
-    InvalidFormat,
-    Expired,
-    InternalError,
-    Unauthorized
-}
-
 #[async_trait]
 impl<'r> FromRequest<'r> for Auth {
 
-    type Error = AuthError;
+    type Error = ();
 
     async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        // Check if exists and format
-        let Some(authorization) = request.authorization() else {
-            return Outcome::Error(
-                (Status::BadRequest, AuthError::MissingAuthorization)
-            )
-        };
-        // Authorization: JWT <JWT_VALUE>
-        let Some(authorization) = extract_jwt(authorization) else {
-            return Outcome::Error(
-                (Status::BadRequest, AuthError::InvalidFormat)
-            )
+        let authorization_jwt = request.authorization()
+            .map(|authorization| authorization_jwt(authorization).ok())
+            .flatten()
+            .flatten();
+
+        let Some(authorization_jwt) = authorization_jwt else {
+            return Outcome::Error((Status::BadRequest, ()))
         };
 
-        // Verify
+        // Preparation for verifying
         let Some(jwt) = request.jwt() else {
-            return Outcome::Error(
-                (Status::InternalServerError, AuthError::InternalError)
-            )
+            return Outcome::Error((Status::InternalServerError, ()))
         };
-
-        let jwt_claims = match jwt.decode(authorization) {
-            Ok(jwt_claims) => { jwt_claims }
-            Err(error) => {
-                let auth_error = match error.kind() {
-                    ErrorKind::ExpiredSignature => { AuthError::Expired }
-                    _ => { AuthError::InvalidFormat }
-                };
-                return Outcome::Error((Status::Unauthorized, auth_error))
-            }
-        };
-
         let Some(mongodb) = request.mongodb() else {
-            return Outcome::Error(
-                (Status::InternalServerError, AuthError::InternalError)
-            )
+            return Outcome::Error((Status::InternalServerError, ()))
+        };
+
+        let Ok(jwt_claims) = jwt.decode(authorization_jwt) else {
+            return Outcome::Error((Status::Unauthorized, ()))
         };
 
         // Verify from database
-        let find_account_result = mongodb.account_token()
+        mongodb.account_token()
             .find_account(jwt_claims.tok, jwt_claims.iat, jwt_claims.exp)
-            .await;
-
-        find_account_result.map(|account| {
-            account.map(|account| Auth(account))
-                .or_error((Status::Unauthorized, AuthError::Unauthorized))
-        }).unwrap_or_else(|_| {
-            Outcome::Error(
-                (Status::Unauthorized, AuthError::InternalError)
-            )
-        })
+            .await
+            .map(|account| account.map(|account| Auth(account)))
+            .ok()
+            .flatten()
+            .or_error((Status::Unauthorized, ()))
     }
 
 }
 
-const AUTHORIZATION_PREFIX: &str = "JWT ";
-fn extract_jwt(authorization: String) -> Option<String> {
-    if !authorization.starts_with(AUTHORIZATION_PREFIX) {
-        return None;
-    }
-    let authorization_header_len = authorization.len();
-    let jwt_prefix_len = AUTHORIZATION_PREFIX.len();
-    if authorization_header_len <= jwt_prefix_len {
-        return None;
-    }
-    let authorization_jwt = authorization[jwt_prefix_len .. authorization_header_len]
-        .to_string();
-    Some(authorization_jwt)
+
+const AUTHORIZATION_REGEX: &str = r"JWT (?<jwt>^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-+/=]*))";
+const AUTHORIZATION_REGEX_JWT: &str = "jwt";
+fn authorization_jwt(authorization: String) -> Result<Option<String>, regex::Error> {
+    let regex = Regex::new(AUTHORIZATION_REGEX)?;
+    let Some(captures) = regex.captures(&authorization) else {
+        return Ok(None)
+    };
+    let jwt = captures.name(AUTHORIZATION_REGEX_JWT)
+        .map(|jwt_match| jwt_match.as_str().to_string());
+    Ok(jwt)
 }
